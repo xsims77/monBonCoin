c83c300f22247f8e0b72ba47b45f74a7a9fa5a0c
xsims77 <simsxiong@gmail.com>
2023-02-13 12:08:01 +0100

    with autoloader

---
+++ autoloader.php
@@ -0,0 +1,8 @@
+<?php
+class Autoload{
+    public static function inclusionAuto($className){
+        require_once __DIR__ . '/' . str_replace('\\', DIRECTORY_SEPARATOR, $className . '.php');
+    }
+}
+
+spl_autoload_register(array('Autoload','inclusionAuto'));
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once('autoloader.php');
+
+?>
+
+<h1>test</h1>
