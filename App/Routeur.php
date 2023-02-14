5da78bffe9b278c13c2a87975d46733e6e32fbae
xsims77 <simsxiong@gmail.com>
2023-02-14 14:58:08 +0100

    with Rooter and htaccess

---
+++ App/Routeur.php
@@ -0,0 +1,62 @@
+<?php
+namespace App;
+
+class Routeur{
+    public function app(){
+        // On teste le routeur
+        // echo "le routeur fonctionne";
+
+        // Récupérer l'url
+        $request = $_SERVER['REQUEST_URI'];
+        // echo $request;
+        // On supprime "/monboncoin/public" de $request
+        $nb = strlen(SITEBASE);
+        $request = substr($request, $nb);
+        // echo "<hr>";
+        // echo $request;
+        // On casse $request pour récupérer uniquement la route demandé et pas les aram GET 
+        $request = explode("?", $request);
+        $request = $request[0];
+        // echo $request;
+
+        // On défini les routes du projet 
+        switch ($request){
+            case '':
+                echo "vous êtes sur la page d'accueil";
+                break;
+            case 'annonces':
+                echo "vous êtes sur la page annonces";
+                break;
+            case 'annonceDetail':
+                echo "vous êtes sur la page détail de l'annonce";
+                break;
+            case 'annonceAjout':
+                echo "vous êtes sur la page création d'annonce";
+                break;
+            case 'annonceModif':
+                echo "vous êtes sur la page de modification d'annonce";
+                break;
+            case 'annonceSuppr':
+                echo "vous êtes sur la page de suppression d'annonce";
+                break;
+            case 'panier':
+                echo "vous êtes sur la page panier";
+                break;
+            case 'inscription':
+                echo "vous êtes sur la page inscription";
+                break;
+            case 'connexion':
+                echo "vous êtes sur la page de connexion";
+                break;
+            case 'deconnexion':
+                echo "vous êtes sur la page de déconnexion";
+                break;
+            case 'profil':
+                echo "vous êtes sur la page de profil";
+                break;
+            default:
+                echo "Cette page n'existe pas ";
+                break;
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 
+use App\Routeur;
+// Ce fichier index est le point d'entrée de notre site
+
 // Création d'une constante pour l'url de base du site
+$config = file_get_contents('../App/config.json');
+$config = json_decode($config);
+$config->baseSite;
+define("SITEBASE", $config->baseSite);
+// echo SITEBASE;
+
+// echo "<hr>";
+
+// Création d'une constante pour la racine du projet
+define('ROOT', dirname(__DIR__));
+// echo ROOT;
+
+// On importe l'autoloader
+require_once(ROOT . DIRECTORY_SEPARATOR . 'autoloader.php');
+
+$routeur = new Routeur;
+$routeur->app();
+
